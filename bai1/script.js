477c20c1dee6b32464eb059c54463bdcf5532d9b
nathanbui170906 <email_dang_ky_github_cua_ban@example.com>
2026-08-17 16:22:24 +0700

    #1 - nhap, xuat du lieu

---
+++ bai1/script.js
@@ -0,0 +1,10 @@
+//          NHẬP XUẤT
+
+// alert : hiển thị thông báo dạng popup trên trình duyệt của người dùng
+alert("You've been banned because you're so cute!");
+
+//. console.log : xuất thông tin ra màn console của trình duyệt, dùng để hiện thông tin debug giúp phát hiện và sửa lỗi trong mã nguồn
+console.log("Banned 1 cute person!");
+
+// prompt : được dùng để hiển thị hộp thoại nhập liệu trên trình duyệt và chờ người dùng nhập giá trị
+prompt("Please enter your name!");
